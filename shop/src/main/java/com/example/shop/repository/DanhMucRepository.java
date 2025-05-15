0e0446a8400e7825028892868cd94bf48b3e99d1
gialong162004 <149921881+gialong162004@users.noreply.github.com>
2025-05-16 00:37:57 +0700

    Delete shop directory

--- shop/src/main/java/com/example/shop/repository/DanhMucRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shop.repository;
-
-import com.example.shop.entity.DanhMuc;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DanhMucRepository extends JpaRepository<DanhMuc, Long> {
-}
--- shop/src/main/java/com/example/shop/repository/SanPhamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shop.repository;
-
-import com.example.shop.entity.SanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SanPhamRepository extends JpaRepository<SanPham, Long> {
-}
